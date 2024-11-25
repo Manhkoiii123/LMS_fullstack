e55f31d380cc991336bb15394a3082489ff636b9
padi-dev-manhtd <manhtd@paditech.com>
2024-11-25 22:02:56 +0700

    add review reply,question reply

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -2,6 +2,10 @@ import express from "express";
 
 import { authorizeRoles, isAuthenticated } from "../middlewares/auth";
 import {
+  addAnswer,
+  addQuestion,
+  addReplyReview,
+  addReview,
   editCourse,
   getAllCourses,
   getCourseByUser,
@@ -27,4 +31,15 @@ courseRouter.get("/get-course/:id", getSingleCourse);
 courseRouter.get("/get-courses", getAllCourses);
 // vào trang khóa học để học (đã mua)
 courseRouter.get("/get-course-content/:id", isAuthenticated, getCourseByUser);
+// add question
+courseRouter.put("/add-question", isAuthenticated, addQuestion);
+courseRouter.put("/reply-question", isAuthenticated, addAnswer);
+courseRouter.put("/add-review/:id", isAuthenticated, addReview);
+courseRouter.put(
+  "/reply-reviews",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  addReplyReview
+);
+
 export default courseRouter;
